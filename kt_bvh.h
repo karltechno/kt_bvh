f04ac7b325e1a9d765677d27a4060c89bf576135
Karl Davis <karl@karltechno.com>
2019-09-14 21:13:20 +0100

    BVH building optimisations
    Separate primitive ID and aabb/origin array for less memory (padding) and better cache locality for use cases. Do all 3 sah bin tests at once for better cache usage when calculating bins

--- kt_bvh.h
+++ kt_bvh.h
@@ -192,7 +192,7 @@ struct BVHBuildDesc
 	}
 
 	// BVH build algorithm.
-	BVHBuildType type = BVHBuildType::SBVH;
+	BVHBuildType type = BVHBuildType::TopDownBinnedSAH;
 
 	// Threshold of primitive to force leaf creation.
 	uint32_t min_leaf_prims = c_default_min_prims_per_leaf;
